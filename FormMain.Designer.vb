5dde22168236c24abd5cb39de77ece33f913eefd
YourAss <yrik.murashko@gmail.com>
2021-04-07 18:59:37 +0300

    Simple serial port reader

--- FormMain.Designer.vb
+++ FormMain.Designer.vb
@@ -22,23 +22,32 @@ Partial Class FormMain
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.ComboBoxCOMPorts = New System.Windows.Forms.ComboBox()
         Me.ButtonConnect = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
         Me.SuspendLayout()
         '
         'ComboBoxCOMPorts
         '
         Me.ComboBoxCOMPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBoxCOMPorts.FormattingEnabled = True
-        Me.ComboBoxCOMPorts.Location = New System.Drawing.Point(11, 14)
+        Me.ComboBoxCOMPorts.Location = New System.Drawing.Point(22, 14)
         Me.ComboBoxCOMPorts.Name = "ComboBoxCOMPorts"
-        Me.ComboBoxCOMPorts.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxCOMPorts.Size = New System.Drawing.Size(100, 21)
         Me.ComboBoxCOMPorts.TabIndex = 0
         '
         'ButtonConnect
         '
-        Me.ButtonConnect.Location = New System.Drawing.Point(138, 14)
+        Me.ButtonConnect.Location = New System.Drawing.Point(128, 14)
         Me.ButtonConnect.Name = "ButtonConnect"
         Me.ButtonConnect.Size = New System.Drawing.Size(99, 21)
         Me.ButtonConnect.TabIndex = 1
@@ -47,16 +56,80 @@ Partial Class FormMain
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(11, 42)
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(22, 53)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(226, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(22, 79)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(22, 105)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 4
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(22, 132)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 5
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Enabled = False
+        Me.TextBox5.Location = New System.Drawing.Point(128, 53)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(99, 20)
+        Me.TextBox5.TabIndex = 9
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Enabled = False
+        Me.TextBox6.Location = New System.Drawing.Point(128, 79)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(99, 20)
+        Me.TextBox6.TabIndex = 8
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Enabled = False
+        Me.TextBox7.Location = New System.Drawing.Point(128, 105)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(99, 20)
+        Me.TextBox7.TabIndex = 7
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Enabled = False
+        Me.TextBox8.Location = New System.Drawing.Point(128, 131)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(99, 20)
+        Me.TextBox8.TabIndex = 6
         '
         'FormMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(249, 73)
+        Me.ClientSize = New System.Drawing.Size(249, 164)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ButtonConnect)
         Me.Controls.Add(Me.ComboBoxCOMPorts)
@@ -70,4 +143,12 @@ Partial Class FormMain
     Friend WithEvents ComboBoxCOMPorts As ComboBox
     Friend WithEvents ButtonConnect As Button
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
 End Class
--- FormMain.vb
+++ FormMain.vb
@@ -1,12 +1,61 @@
-﻿Public Class FormMain
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO.Ports
+
+Public Class FormMain
+    Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Получение списка доступных COM портов
-        For Each sp As String In My.Computer.Ports.SerialPortNames
-            ComboBoxCOMPorts.Items.Add(sp)
-        Next
+        Try
+            For Each sp As String In My.Computer.Ports.SerialPortNames
+                ComboBoxCOMPorts.Items.Add(sp)
+            Next
+            'Выбрать первый по-умолчанию
+            ComboBoxCOMPorts.SelectedIndex = 0
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+        'Если не подключен
+        If Not SerialPort.IsOpen Then
+            Try
+                'Установка параметров и подключение
+                SerialPort.PortName = ComboBoxCOMPorts.SelectedItem
+                SerialPort.Open()
 
+                'Изменение интерфейса после подключения
+                ButtonConnect.Text = "Отключиться"
+                TextBox1.Enabled = True
+                TextBox2.Enabled = True
+                TextBox3.Enabled = True
+                TextBox4.Enabled = True
+                TextBox5.Enabled = True
+                TextBox6.Enabled = True
+                TextBox7.Enabled = True
+                TextBox8.Enabled = True
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            'Отключение
+            SerialPort.Close()
+
+            'Изменение интерфейса после отключения
+            ButtonConnect.Text = "Подключиться"
+            TextBox1.Enabled = False
+            TextBox2.Enabled = False
+            TextBox3.Enabled = False
+            TextBox4.Enabled = False
+            TextBox5.Enabled = False
+            TextBox6.Enabled = False
+            TextBox7.Enabled = False
+            TextBox8.Enabled = False
+        End If
+
+    End Sub
+
+    'Чтение данных из COM-порта
+    Private Sub SerialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        'Вывод полученных данных
+        Invoke(Sub() TextBox1.Text = SerialPort.ReadExisting())
     End Sub
 End Class
