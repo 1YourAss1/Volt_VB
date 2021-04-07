f855d8cfe467c4c74d657471c7fdf3fc0fee7ad2
YourAss <yrik.murashko@gmail.com>
2021-04-07 16:49:56 +0300

    Добавьте файлы проекта.

---
+++ FormMain.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ComboBoxCOMPorts = New System.Windows.Forms.ComboBox()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ComboBoxCOMPorts
+        '
+        Me.ComboBoxCOMPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBoxCOMPorts.FormattingEnabled = True
+        Me.ComboBoxCOMPorts.Location = New System.Drawing.Point(11, 14)
+        Me.ComboBoxCOMPorts.Name = "ComboBoxCOMPorts"
+        Me.ComboBoxCOMPorts.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxCOMPorts.TabIndex = 0
+        '
+        'ButtonConnect
+        '
+        Me.ButtonConnect.Location = New System.Drawing.Point(138, 14)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(99, 21)
+        Me.ButtonConnect.TabIndex = 1
+        Me.ButtonConnect.Text = "Подключиться"
+        Me.ButtonConnect.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(11, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(226, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 73)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ButtonConnect)
+        Me.Controls.Add(Me.ComboBoxCOMPorts)
+        Me.Name = "FormMain"
+        Me.Text = "Volt"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBoxCOMPorts As ComboBox
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ FormMain.vb
@@ -0,0 +1,12 @@
+﻿Public Class FormMain
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Получение списка доступных COM портов
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBoxCOMPorts.Items.Add(sp)
+        Next
+    End Sub
+
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+
+    End Sub
+End Class
